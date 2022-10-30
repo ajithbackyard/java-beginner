85a5063cfbf63620b38da72480df504abaa49c75
Rishicalicut <56603545+Rishicalicut@users.noreply.github.com>
2022-10-30 19:24:44 +0530

    Create MaxNUmberOfHandshake.java

---
+++ MaxNUmberOfHandshake.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	     //fill the code
+    int num = 10;
+
+    int total = num * (num-1) / 2; // Combination nC2
+
+		System.out.println("For "+ num +" people there will be " +total+" handshakes");
+	}
+}
